26e0bebafafd494ab4105f2b47b4fe5f0d2aa3b7
Rafael Sales <fco.rsales@gmail.com>
2024-09-14 17:07:50 +0000

    2Â3 commit

--- SRC/calculadora.js
+++ SRC/calculadora.js
@@ -3,7 +3,13 @@ function add(a, b) {
 return a + b; 
  }
  
-// Exportar as funções 
- module.exports = { 
- add 
- }; 
+// Função de subtração 
+  function subtract(a, b) { 
+    return a - b; 
+  } 
+ 
+  // Exportar as funções 
+  module.exports = { 
+    add, 
+    subtract 
+  }; 
